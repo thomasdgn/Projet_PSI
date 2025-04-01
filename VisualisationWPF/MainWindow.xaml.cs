99ecdb16e89d58d2c15c0608e0eddcc15d0c362d
thomasdgn <degardin.thom@gmail.com>
2025-04-01 14:33:53 +0200

    Création
    Création d'un nouveau dépôt contenant l'interface graphique

---
+++ VisualisationWPF/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Collections.Specialized.BitVector32;
+
+namespace VisualisationWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Graphe<Station> graphe;
+        public MainWindow(Graphe<Station> grapheRecu)
+        {
+            InitializeComponent();
+            graphe = grapheRecu;
+        }
+    }
+}
